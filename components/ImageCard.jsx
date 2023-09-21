3a34ac131005a4779988c204927362d4432d8104
Florentina <florentinaantigha27@gmail.com>
2023-09-21 15:13:52 +0100

    Implemented Loading state

--- components/ImageCard.jsx
+++ components/ImageCard.jsx
@@ -1,22 +1,29 @@
 import Image from 'next/image';
 import React from 'react'
+import { CSS } from '@dnd-kit/utilities';
+import { useSortable } from '@dnd-kit/sortable';
 
 const ImageCard = ({image}) => {
+    const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id: image.id });
+	const style = {
+		transform: CSS.Transform.toString(transform),
+		transition,
+	};
     console.log(image.id)
     return (
-		<section className={`  shadow-md p-2  cursor-grab touch-manipulation`}>
+		<section ref={setNodeRef} style={style} {...attributes} {...listeners} className={`  shadow-md p-2  cursor-grab touch-manipulation`}>
 			<div className='block relative xs:h-[120px] sm:h-[150px]  md:h-[200px] lg:h-[250px] rounded-xl shadow-lg '>
-				<Image placeholder='empty' blurDataURL={image?.blur_hash || 'LJJ*esOZ?w-;~XsmaJxvT1t6jEbI'} src={image?.urls?.small} layout='fill' objectFit='cover' alt='image' className={`rounded-md aspect-square bg-blend-color bg-center`} />
+				<Image placeholder='empty' blurDataURL={image?.blur_hash || 'LJJ*esOZ?w-;~XsmaJxvT1t6jEbI'} src={image?.urls?.small} layout='fill' objectFit='cover' alt='image' className={`${isDragging ? 'opacity-50 bg-[rgba(0,0,0,0.8)]' : 'opacity-100'} rounded-md aspect-square bg-blend-color bg-center`} />
 			</div>
 			<ul className='flex w-full bg-white gap-1 justify-start flex-nowrap'>
 				{image?.tags?.slice(0, 2).map((tag) => (
-					<li key={tag?.title.slice(0, 7)} className='text-gray-600 shadow-md text-[12px] rounded-md border border-gray-200 md:text-[14px] lg:text-base p-1 py-0 mt-1'>
+					<li key={tag?.title.slice(0, 7)} className='shadow-md text-gray-600 text-[12px] rounded-md border border-gray-200 md:text-[14px] lg:text-base p-1 py-0 mt-1'>
 						{tag?.title.slice(0, 10)}
 					</li>
 				))}
 			</ul>
 		</section>
 	);
-}
+};
 
 export default ImageCard
--- components/ImageGallery.jsx
+++ components/ImageGallery.jsx
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ImageCard from "./ImageCard";
+import { DndContext, closestCenter, KeyboardSensor, MouseSensor, TouchSensor, useSensor, useSensors } from '@dnd-kit/core';
+import { arrayMove, SortableContext, verticalListSortingStrategy, reactSwappingStrategy } from '@dnd-kit/sortable';
+import { toast } from "react-toastify";
+import { RotatingLines } from "react-loader-spinner";
+import { useSession } from "next-auth/react";
 
 const accessKey = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY;
 
 const ImageGallery = () => {
+const { data: session, status } = useSession();
+const mouseSensor = useSensor(MouseSensor);
+const touchSensor = useSensor(TouchSensor);
+const keyboardSensor = useSensor(KeyboardSensor);
+const sensors = useSensors(mouseSensor, touchSensor, keyboardSensor);
   const [items, setItems] = useState([]);
   const [imageLoading, setImageLoading] = useState([]);
   const searchTerm = "Interior design";
   const fetchImages = async () => {
     setImageLoading(true);
-    const { data } = await axios.get("https://api.unsplash.com/search/photos", {
+    try {
+        const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: {
         query: searchTerm,
         per_page: 25,
@@ -19,24 +30,64 @@ const ImageGallery = () => {
         Authorization: `Client-ID ${accessKey}`,
       },
     });
+    if(!response.error){
+    const {data}=response
     console.log(data);
     setItems(data.results);
+    setImageLoading(false)
+    return 
+ }
+    throw new Error("Could not fetch images")
+    } catch (error) {
+      toast.error(error.message)
+      setImageLoading(false)
+    }
   };
   useEffect(() => {
-    if (accessKey) {
+    if (accessKey && session?.user?.email) {
       fetchImages();
     }
     console.log(accessKey);
-  }, [searchTerm, accessKey]);
-  return (
-    <div className="w-full">
-      <section className='grid xs:grid-cols-2 sm:grid-cols-3 touch-pan-y md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 w-full justify-center px-8 md:px-12 lg:px-20'>
+  }, [searchTerm, accessKey, session?.user]);
+
+	useEffect(() => {
+		setItems(items);
+		console.log(items);
+	}, [items]);
+
+	const handleDragEnd = (event) => {
+		const { active, over } = event;
+		const oldIndex = items.findIndex((p) => p.id === active.id);
+		const newIndex = items.findIndex((p) => p.id === over.id);
+		setItems((items) => arrayMove(items, oldIndex, newIndex));
+	};
+    return (
+		<main className='w-full min-h-screen'>
+			{imageLoading ? (
+				<section className='w-full flex justify-center'>
+                <RotatingLines strokeColor='white' strokeWidth='5' animationDuration='0.75' width='30' visible={true} />
+				</section>
+			) : (
+				<>
+					{items?.length === 0 ? (
+						<section>
+							<h1 className='text-2xl font-bold text-center mt-10'>No images found. Search another!!!</h1>
+						</section>
+					) : (
+						<DndContext autoScroll={{ acceleration: 1 }} TouchSensor collisionDetection={closestCenter} onDragEnd={handleDragEnd} sensors={sensors}>
+							<SortableContext items={items} strategy={reactSwappingStrategy}>
+								<section className='grid xs:grid-cols-2 sm:grid-cols-3 touch-pan-y md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 w-full justify-center px-8 md:px-12 lg:px-20'>
 									{items?.map((image, index) => (
 										<ImageCard key={image.id} image={image} />
 									))}
 								</section>
-    </div>
-  );
+							</SortableContext>
+						</DndContext>
+					)}
+				</>
+			)}
+		</main>
+	);
 };
 
 export default ImageGallery;
